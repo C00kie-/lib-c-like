41b4a12e78e7c2537192264b02050ebf7a383ae0
C00kie- <pbourmea@student.42.fr>
2017-12-29 21:28:34 +0100

    Update get_next_line.c

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -12,52 +12,53 @@
 
 #include "get_next_line.h"
 
-static char		*boucle_if(char *tmp, char *str)
+static char		*join_buffer(char *buf, char *str)
 {
-	char			*tempo;
+	char			*new_str;
 
-	tempo = ft_strjoin(tmp, str);
+	new_str = ft_strjoin(buf, str);
 	ft_bzero(str, BUF_SIZE);
-	free(tmp);
-	return (tempo);
+	free(buf);
+	return (new_str);
 }
 
-static int		other_if(int i, char *tmp, char **line, char **statik)
+static int		Eof(int i, char *buf, char **line, char **ret)
 {
 	int				len;
 
-	len = ft_strlen(tmp);
-	tmp[i++] = '\0';
+	len = ft_strlen(buf);
+	buf[i++] = '\0';
 	if (len - i >= 0)
-		*statik = ft_strsub(tmp, i, len - i);
-	*(line) = ft_strdup(tmp);
+		*ret = ft_strsub(buf, i, len - i);
+	*(line) = ft_strdup(buf);
+	free(buf);
 	return (1);
 }
 
 int				get_next_line(int fd, char **line)
 {
 	char			*str;
-	char			*tmp;
+	char			*buf;
 	int				i;
 	int				len;
-	static char		*statik = NULL;
+	static char		*ret = NULL;
 
 	if ((str = ft_strnew(BUF_SIZE + 1)) == NULL || line == NULL)
 		return (-1);
-	tmp = statik;
-	statik = NULL;
+	buf = ret;
+	ret = NULL;
 	*line = NULL;
 	while ((len = read(fd, str, BUF_SIZE)) >= 0)
 	{
-		if ((len == 0 && (tmp == NULL || tmp[0] == '\0')))
+		if ((len == 0 && (buf == NULL || buf[0] == '\0')))
 			return (0);
 		if (len != 0)
-			tmp = boucle_if(tmp, str);
+			buf = join_buffer(buf, str);
 		i = 0;
-		while ((tmp[i] != '\n') && (tmp[i] != '\0'))
+		while ((buf[i] != '\n') && (buf[i] != '\0'))
 			i++;
-		if (i != (int)ft_strlen(tmp) || len == 0)
-			return (other_if(i, tmp, line, &statik));
+		if (i != (int)ft_strlen(buf) || len == 0)
+			return (Eof(i, buf, line, &ret));
 	}
 	return (len);
 }
